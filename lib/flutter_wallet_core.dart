ca3bac0c7c7f99e9f8b2c555efc5d1df2f173a5a
ZhangRGK <stamen0630@gmail.com>
2020-05-15 16:08:02 +0800

    add dart part and unnecessary unit test

--- lib/flutter_wallet_core.dart
+++ lib/flutter_wallet_core.dart
@@ -2,12 +2,47 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-class FlutterWalletCore {
+class Keys {
+  final String publicKey;
+  final String address;
+
+  Keys({this.publicKey, this.address});
+}
+
+class WalletCore {
   static const MethodChannel _channel =
       const MethodChannel('flutter_wallet_core');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future<String> generateMnemonic() async {
+    final String mnemonic =
+        await _channel.invokeMethod<String>('generateMnemonic');
+    return mnemonic;
+  }
+
+  static Future<Map<String, Keys>> importMnemonic(String mnemonic, String path,
+      String password, List<String> symbols) async {
+    final Map<String, Map<String, String>> keyInfo = await _channel
+        .invokeMethod<Map<String, Map<String, String>>>('importMnemonic', {
+      "mnemonic": mnemonic,
+      "path": path,
+      "password": password,
+      "symbols": symbols.join(",")
+    });
+
+    return keyInfo.map((key, value) => MapEntry(
+        key, Keys(address: value["address"], publicKey: value["publicKey"])));
+  }
+
+  static Future<String> signTx(String mnemonic, String path, String password,
+      String symbol, String rawTx) async {
+    final String signedTx = await _channel.invokeMethod<String>("signTx", {
+      "mnemonic": mnemonic,
+      "path": path,
+      "password": password,
+      "symbol": symbol,
+      "rawTx": rawTx,
+    });
+
+    return signedTx;
   }
 }
--- test/flutter_wallet_core_test.dart
+++ test/flutter_wallet_core_test.dart
@@ -1,23 +1,106 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_wallet_core/flutter_wallet_core.dart';
 
 void main() {
-  const MethodChannel channel = MethodChannel('flutter_wallet_core');
-
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  setUp(() {
-    channel.setMockMethodCallHandler((MethodCall methodCall) async {
-      return '42';
+  test('#generateMnemonic', () async {
+    String mnemonic = await WalletCore.generateMnemonic();
+    expect(mnemonic, isNotNull);
+    expect(mnemonic.length, isNot(0));
+  });
+
+  group("#importMnemonic", () {
+    final String password = "test_flutter_wallet_core";
+    List<String> symbols = ["BTC", "ETH", "BBC", "USDT"];
+
+    test("with default path", () async {
+      final String mnemonic = await WalletCore.generateMnemonic();
+      final String path = "m/44'/0'/0'/0/0";
+      Map<String, Keys> keyInfo =
+          await WalletCore.importMnemonic(mnemonic, path, password, symbols);
+
+      expect(keyInfo.keys.toList(), symbols);
+
+      for (String symbol in keyInfo.keys) {
+        final Keys keys = keyInfo[symbol];
+
+        expect(keys.publicKey, isNotNull);
+        expect(keys.address, isNotNull);
+      }
+    });
+
+    test("with dabank legecy path", () async {
+      final String mnemonic = await WalletCore.generateMnemonic();
+      final String path = "m/44'/%d'";
+      Map<String, Keys> keyInfo =
+          await WalletCore.importMnemonic(mnemonic, path, password, symbols);
+
+      expect(keyInfo.keys.toList(), symbols);
+
+      for (String symbol in keyInfo.keys) {
+        final Keys keys = keyInfo[symbol];
+
+        expect(keys.publicKey, isNotNull);
+        expect(keys.address, isNotNull);
+      }
+    });
+
+    test("with custom path", () async {
+      final String mnemonic = await WalletCore.generateMnemonic();
+      final String path = "m/44'/0'/1'/0/0";
+      Map<String, Keys> keyInfo =
+          await WalletCore.importMnemonic(mnemonic, path, password, symbols);
+
+      expect(keyInfo.keys.toList(), symbols);
+
+      for (String symbol in keyInfo.keys) {
+        final Keys keys = keyInfo[symbol];
+
+        expect(keys.publicKey, isNotNull);
+        expect(keys.address, isNotNull);
+      }
     });
   });
 
-  tearDown(() {
-    channel.setMockMethodCallHandler(null);
-  });
+  group("#signTx", () {
+    final String password = "test_flutter_wallet_core";
+    final String path = "m/44'/0'/0'/0/0";
 
-  test('getPlatformVersion', () async {
-    expect(await FlutterWalletCore.platformVersion, '42');
+    test("sign BTC tx", () async {
+      final String symbol = "BTC";
+      final String mnemonic = await WalletCore.generateMnemonic();
+      final String rawTx = "test_tx";
+      final String signTx =
+          await WalletCore.signTx(mnemonic, path, password, symbol, rawTx);
+      expect(signTx, isNotNull);
+    });
+
+    test("sign ETH tx", () async {
+      final String symbol = "BTC";
+      final String mnemonic = await WalletCore.generateMnemonic();
+      final String rawTx = "test_tx";
+      final String signTx =
+          await WalletCore.signTx(mnemonic, path, password, symbol, rawTx);
+      expect(signTx, isNotNull);
+    });
+
+    test("sign BBC tx", () async {
+      final String symbol = "BTC";
+      final String mnemonic = await WalletCore.generateMnemonic();
+      final String rawTx = "test_tx";
+      final String signTx =
+          await WalletCore.signTx(mnemonic, path, password, symbol, rawTx);
+      expect(signTx, isNotNull);
+    });
+
+    test("sign USDT tx", () async {
+      final String symbol = "BTC";
+      final String mnemonic = await WalletCore.generateMnemonic();
+      final String rawTx = "test_tx";
+      final String signTx =
+          await WalletCore.signTx(mnemonic, path, password, symbol, rawTx);
+      expect(signTx, isNotNull);
+    });
   });
 }
