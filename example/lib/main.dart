1e3d33f5793f49729edb4487b132406aaa234458
ZhangRGK <stamen0630@gmail.com>
2020-05-18 10:17:25 +0800

    implements dart part

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
 
-import 'package:flutter/services.dart';
 import 'package:flutter_wallet_core/flutter_wallet_core.dart';
 
 void main() {
@@ -14,31 +13,18 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
+  String mnemonic;
 
   @override
   void initState() {
     super.initState();
-    initPlatformState();
   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await FlutterWalletCore.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
+  void generateMnemonic() async {
+    final String mnemonic = await WalletCore.generateMnemonic();
 
     setState(() {
-      _platformVersion = platformVersion;
+      this.mnemonic = mnemonic;
     });
   }
 
@@ -46,13 +32,18 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Plugin example app'),
-        ),
-        body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
-      ),
+          appBar: AppBar(
+            title: const Text('Flutter wallet core example app'),
+          ),
+          body: SingleChildScrollView(
+            child: Column(
+              children: <Widget>[
+                FlatButton(
+                    child: Text("Generate Mnemonic"),
+                    onPressed: generateMnemonic)
+              ],
+            ),
+          )),
     );
   }
 }
