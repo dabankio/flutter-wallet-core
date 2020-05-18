eee6902da3d735015a5d2cc1eeeba1f4c7fe6979
ZhangRGK <stamen0630@gmail.com>
2020-05-18 10:17:48 +0800

    modify ios part

--- ios/Classes/FlutterWalletCorePlugin.m
+++ ios/Classes/FlutterWalletCorePlugin.m
@@ -12,24 +12,44 @@
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"generateMnemonic" isEqualToString:call.method]) {
-    NSString* mnemonic = generateMnemonic();
+    NSError* error;
+    NSString* mnemonic = [FlutterWalletCorePlugin generateMnemonic, error:&error];
+
+    if (error) {
+      result(error);
+      return;
+    }
     result(mnemonic);
   } else if ([@"importMnemonic" isEqualToString:call.method]) {
     NSString* mnemonic = call.arguments[@"mnemonic"];
     NSString* path = call.arguments[@"path"];
     NSString* password = call.arguments[@"password"];
     NSString* symbolString = call.arguments[@"symbolString"];
-    WalletWallet* wallet = getWalletInstance(mnemonic, path, password);
+    NSError* error;
+    WalletWallet* wallet = [FlutterWalletCorePlugin getWalletInstance:mnemonic path:path password:password, error:&error];
 
-    NSArray *symbols = [string componentsSeparatedByString: @","];
-    NSMutableDictionary keyInfo = [NSMutableDictionary dictionaryWithCapacity:symbols.length];
+    if (error) {
+      result(error);
+      return;
+    }
+
+    NSArray *symbols = [symbolString componentsSeparatedByString: @","];
+    NSMutableDictionary *keyInfo = [NSMutableDictionary dictionaryWithCapacity:[symbols count]];
 
     for (NSString *symbol in symbols) {
-      NSMutableDictionary keys = [NSMutableDictionary dictionaryWithCapacity:2];
-      keys[@"publicKey"] = wallet.derivePublicKey(symbol);
-      keys[@"address"] = wallet.deriveAddress(symbol);
+      NSMutableDictionary *keys = [NSMutableDictionary dictionaryWithCapacity:2];
+      keys[@"publicKey"] = [wallet derivePublicKey:symbol error:&error];
+      if (error) {
+        result(error);
+        return;
+      } 
+      keys[@"address"] = [wallet deriveAddress:symbol error:&error];
+      if (error) {
+        result(error);
+        return;
+      } 
 
-      keyInfo[@symbol] = keys;
+      keyInfo[symbol] = keys;
     }
     result(keyInfo);
   } else if ([@"signTx" isEqualToString:call.method]) {
@@ -38,30 +58,26 @@
     NSString* password = call.arguments[@"password"];
     NSString* symbol = call.arguments[@"symbol"];
     NSString* rawTx = call.arguments[@"rawTx"];
+    NSError* error;
 
-    NSString* signedTx = signTx(mnemonic, path, password, symbol, rawTx);
+    NSString* signedTx = [FlutterWalletCorePlugin signTx:mnemonic path:path password:password symbol:symbol rawTx:rawTx, error:&error];
 
+    if (error) {
+      result(error);
+      return;
+    }
     result(signedTx);
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
 
-- (NSString*)generateMnemonic {
-  NSError * __autoreleaseing error;
-
-  NSString* menmonic WalletNewMnemonic(&error);
-
-  if (error) {
-    result(error);
-  } else {
-    result(mnemonic);
-  }
++ (NSString*)generateMnemonic:(NSError* _Nullable* _Nullable)error {
+  NSString* menmonic = WalletNewMnemonic(&error);
+  return mnemonic;
 }
 
-- (WalletWallet*) getWalletInstance:(NSString*)mnemonic path:(NSString*)path password:(NSString*)password {
-    NSError * __autoreleaseing error;
-
++ (WalletWallet*) getWalletInstance:(NSString*)mnemonic path:(NSString*)path password:(NSString*)password error:(NSError* _Nullable* _Nullable)error {
     id<WalletWalletOptions> options;
     options.add(WalletWithPathFormat(path));
     options.add(WalletWithPassword(password));
@@ -70,9 +86,9 @@
     return wallet;
 }
 
-- (NSString*) signTx:(NSString*)mnemonic path:(NSString*)path password:(NSString*)password symbol:(NSString*)symbol rawTx:(NSString*)rawTx {
-    WalletWallet* wallet = getWalletInstance(mnemonic, path, password);
-    return wallet.sign(symbol, rawTx);
++ (NSString*) signTx:(NSString*)mnemonic path:(NSString*)path password:(NSString*)password symbol:(NSString*)symbol rawTx:(NSString*)rawTx error:(NSError* _Nullable* _Nullable)error {
+    WalletWallet* wallet = getWalletInstance(mnemonic, path, password, &error);
+    return wallet.sign(symbol, rawTx, &error);
 }
 
 @end
