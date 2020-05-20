8ffb3bdb302b6d18140190e2685c8cc1ac47445b
ZhangRGK <stamen0630@gmail.com>
2020-05-20 12:26:27 +0800

    validate mnemonic before import

--- ios/Classes/FlutterWalletCorePlugin.m
+++ ios/Classes/FlutterWalletCorePlugin.m
@@ -16,7 +16,7 @@
     NSString* mnemonic = [FlutterWalletCorePlugin generateMnemonic:&error];
 
     if (error) {
-      result(error);
+      result([FlutterError errorWithCode:@"generateMnemonicError" message:error.localizedDescription details:nil]);
       return;
     }
     result(mnemonic);
@@ -26,36 +26,42 @@
     NSString* password = call.arguments[@"password"];
     NSString* symbolString = call.arguments[@"symbols"];
     NSError* __autoreleasing error;
+
+    WalletValidateMnemonic(mnemonic, &error);
+
+    if (error) {
+      result([FlutterError errorWithCode:@"PARAMETER_ERROR" message:@"Mnemonic is invalid" details:nil]);
+      return;
+    }
+
     WalletWallet* wallet = [FlutterWalletCorePlugin getWalletInstance:mnemonic path:path password:password error:&error];
 
     if (error) {
-      result(error);
+      result([FlutterError errorWithCode:@"importMnemonicError" message:error.localizedDescription details:nil]);
       return;
     }
 
     NSString* publicKey = [wallet derivePublicKey:@"BTC" error:&error];
 
-    result(publicKey);
+    NSArray *symbols = [symbolString componentsSeparatedByString: @","];
+    NSMutableDictionary *keyInfo = [NSMutableDictionary dictionaryWithCapacity:[symbols count]];
 
-//     NSArray *symbols = [symbolString componentsSeparatedByString: @","];
-//     NSMutableDictionary *keyInfo = [NSMutableDictionary dictionaryWithCapacity:[symbols count]];
+    for (NSString *symbol in symbols) {
+      NSMutableDictionary *keys = [NSMutableDictionary dictionaryWithCapacity:2];
+      keys[@"publicKey"] = [wallet derivePublicKey:symbol error:&error];
+      if (error) {
+        result([FlutterError errorWithCode:@"derivePublicKeyError" message:error.localizedDescription details:nil]);
+        return;
+      } 
+      keys[@"address"] = [wallet deriveAddress:symbol error:&error];
+      if (error) {
+        result([FlutterError errorWithCode:@"deriveAddressError" message:error.localizedDescription details:nil]);
+        return;
+      } 
 
-//     for (NSString *symbol in symbols) {
-//       NSMutableDictionary *keys = [NSMutableDictionary dictionaryWithCapacity:2];
-//       keys[@"publicKey"] = [wallet derivePublicKey:symbol error:&error];
-//       if (error) {
-//           result([FlutterError errorWithCode:@"derivePublicKeyError" message:error.localizedDescription details:nil]);
-//         return;
-//       } 
-//       keys[@"address"] = [wallet deriveAddress:symbol error:&error];
-//       if (error) {
-// //          result([FlutterError errorWithCode:error.code]);
-//         return;
-//       } 
-
-//       keyInfo[symbol] = keys;
-//     }
-//     result(keyInfo);
+      keyInfo[symbol] = keys;
+    }
+    result(keyInfo);
   } else if ([@"signTx" isEqualToString:call.method]) {
     NSString* mnemonic = call.arguments[@"mnemonic"];
     NSString* path = call.arguments[@"path"];
@@ -64,10 +70,17 @@
     NSString* rawTx = call.arguments[@"rawTx"];
     NSError* __autoreleasing error;
 
+    WalletValidateMnemonic(mnemonic, &error);
+
+    if (error) {
+      result([FlutterError errorWithCode:@"PARAMETER_ERROR" message:@"Mnemonic is invalid" details:nil]);
+      return;
+    }
+
     NSString* signedTx = [FlutterWalletCorePlugin signTx:mnemonic path:path password:password symbol:symbol rawTx:rawTx error:&error];
 
     if (error) {
-//      result([FlutterError errorWithCode:error.code]);
+      result([FlutterError errorWithCode:@"signError" message:error.localizedDescription details:nil]);
       return;
     }
     result(signedTx);
@@ -84,7 +97,7 @@
 + (WalletWallet*) getWalletInstance:(NSString*)mnemonic path:(NSString*)path password:(NSString*)password error:(NSError * _Nullable __autoreleasing * _Nullable)error {
     WalletWalletOptions* options = [WalletWalletOptions new];
     id<WalletWalletOption> pathOption = WalletWithPathFormat(path);
-    id<WalletWalletOption> passwordOption = WalletWithPathFormat(password);
+    id<WalletWalletOption> passwordOption = WalletWithPassword(password);
     [options add:pathOption];
     [options add:passwordOption];
 
